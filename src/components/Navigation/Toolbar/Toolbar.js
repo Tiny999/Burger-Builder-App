0c6f8ef4b9e090a00dd13eae89e1a3f214127d37
Tiny999 <salamiabbas3@gmail.com>
2020-08-29 00:19:31 +0100

    'add-navigation-links'

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
       <div>Menu</div>
-      <div>Logo</div>
+      <Logo/>
       <ul>
-        ...
+        <NavItems/>
       </ul>
   </header>
 );
