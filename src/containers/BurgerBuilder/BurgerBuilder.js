2755e53e0129d59691f6350e39e29d5929fd4db4
Tiny999 <salamiabbas3@gmail.com>
2020-09-24 22:38:26 +0100

    order-button-and-checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,9 +33,9 @@ class BurgerBuilder extends Component{
       return ingredients[igKey];
     }).reduce((sum, el) => {
       return sum + el;
-    }, 0)
+    }, 0);
 
-    this.setState({purchasable: sum > 0 });
+    return sum > 0;
   }
 
 
@@ -48,19 +48,7 @@ class BurgerBuilder extends Component{
   }
 
   purchaseContinuedHandler = () => {
-    const queryParams = [];
-
-    for (let i in this.state.ingredients){
-      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-    };
-
-    queryParams.push(`price= ${this.state.totalPrice}`);
-    const queryString = queryParams.join('&');
-
-    this.props.history.push({
-      pathname: '/checkout',
-      search: `?${queryString}`
-    });
+    this.props.history.push('/checkout');
   }
 
   render(){
@@ -83,7 +71,7 @@ class BurgerBuilder extends Component{
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseStatus(this.props.ings)}
             ordered = {this.buyingHandler}
             price={this.props.price}/>
         </Aux>
